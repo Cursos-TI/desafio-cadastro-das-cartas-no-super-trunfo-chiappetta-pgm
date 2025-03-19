a19d2aa7410e333163594814f220a58249384f9a
chiappetta-pgm <leandro_chiappetta@yahoo.com.br>
2025-03-19 19:37:21 +0000

    final scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,7 @@
 int main(){
     // Primeira carta//
    printf("super trunfo \n");
+   printf("Carta 1 \n");
    char estado[50];
    char cidade[50];
    char codigo[50];
@@ -23,17 +24,43 @@ int main(){
    scanf("%s", &codigo);
    
    printf("População: \n");
-   scanf("%f, &populacao");
+   scanf("%e, &populacao");
 
    printf("Pontos Turísticos: \n");
    scanf("%i, &turisticos");
 
    printf("Área em m²: \n");
-   scanf("%f, &area");
+   scanf("%e, &area");
 
    printf("PIB: \n");
-   scanf("%f, &pib");
+   scanf("%e, &pib \n");
+
+     
+   //inserção dados segunda carta//
 
 
+   printf("Carta 2 \n");
+
+   printf("Estado: \n");
+   scanf("%s", estado);
+
+   printf("Cidade: \n");
+   scanf("%s", &cidade);
+
+   printf("Código da carta: \n");
+   scanf("%s", &codigo);
+   
+   printf("População: \n");
+   scanf("%e, &populacao");
+
+   printf("Pontos Turísticos: \n");
+   scanf("%i, &turisticos");
+
+   printf("Área em m²: \n");
+   scanf("%e, &area");
+
+   printf("PIB: \n");
+   scanf("%e, &pib \n");
+   
     return 0;
 }
